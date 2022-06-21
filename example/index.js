9ee97cac667336e3ca2db3d9730bb918d3c1bbc8
肖舟【家里】 <993749927@qq.com>
2022-06-22 00:42:55 +0800

    可运行js文件

--- example/index.js
+++ example/index.js
@@ -2,17 +2,15 @@ import Vue from 'vue'
 import MonacoEditor from '../src'
 import './style.css'
 
-
-
 function Route(url) {
   this.query = {}
   if (url.indexOf('?') === -1) {
     return
   }
   const query = url.split('?')[1]
-  var vars = query.split("&");
+  var vars = query.split('&')
   for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split("=");
+    var pair = vars[i].split('=')
     this.query[pair[0]] = pair[1]
   }
 }
@@ -34,39 +32,41 @@ new Vue({
 
   methods: {
     renderDiff() {
-      return <div>
+      return (
+        <div>
+          <div v-show={!this.showDiff} class="diff-split-editor">
+            <MonacoEditor
+              class="editor"
+              value={this.original}
+              language={this.language}
+              theme={this.theme}
+              options={this.options}
+              onChange={newValue => (this.original = newValue)}
+            />
+            <MonacoEditor
+              class="editor"
+              value={this.code}
+              language={this.language}
+              theme={this.theme}
+              options={this.options}
+              onChange={newValue => (this.code = newValue)}
+            />
+          </div>
 
-        <div v-show={!this.showDiff} class="diff-split-editor">
-          <MonacoEditor
-            class="editor"
-            value={this.original}
-            language={this.language}
-            theme={this.theme}
-            options={this.options}
-            onChange={newValue => (this.original = newValue)}
-          />
-          <MonacoEditor
-            class="editor"
-            value={this.code}
-            language={this.language}
-            theme={this.theme}
-            options={this.options}
-            onChange={newValue => (this.code = newValue)}
-          />
+          {this.showDiff ? (
+            <MonacoEditor
+              class="full-editor"
+              diffEditor={true}
+              original={this.original}
+              value={this.code}
+              language={this.language}
+              theme={this.theme}
+              options={this.options}
+              onChange={newValue => (this.code = newValue)}
+            />
+          ) : null}
         </div>
-
-        {this.showDiff ? <MonacoEditor
-          class="full-editor"
-          diffEditor={true}
-          original={this.original}
-          value={this.code}
-          language={this.language}
-          theme={this.theme}
-          options={this.options}
-          onChange={newValue => (this.code = newValue)}
-        /> : null}
-
-      </div>
+      )
     }
   },
   created() {
@@ -82,33 +82,56 @@ new Vue({
     return (
       <div id="app">
         <div class="header">
-
           <div class="title">在线{this.diff ? 'diff' : ''}编辑器</div>
 
           <div class="btns">
-            {this.diff ?
-              (this.showDiff ?
-                <button class="btn" onClick={() => this.showDiff = !this.showDiff}>去编辑</button>
-                :
-                <button class="btn btn-primary" onClick={() => this.showDiff = !this.showDiff}>开始比较</button>) : null}
+            {this.diff ? (
+              this.showDiff ? (
+                <button
+                  class="btn"
+                  onClick={() => (this.showDiff = !this.showDiff)}
+                >
+                  去编辑
+                </button>
+              ) : (
+                <button
+                  class="btn btn-primary"
+                  onClick={() => (this.showDiff = !this.showDiff)}
+                >
+                  开始比较
+                </button>
+              )
+            ) : null}
 
-            {!this.diff && this.language === 'javascript' ?
-              <button onClick={() => {
-                alert('开发中~')
-              }} class="btn">运行代码</button> : null}
+            {!this.diff && this.language === 'javascript' ? (
+              <button
+                onClick={() => {
+                  try {
+                    eval(this.code)
+                  } catch (error) {
+                    console.log(error)
+                  }
+                }}
+                class="btn"
+              >
+                运行代码
+              </button>
+            ) : null}
           </div>
         </div>
 
-        {!this.diff ? <MonacoEditor
-          class="full-editor"
-          value={this.code}
-          language={this.language}
-          theme={this.theme}
-          options={this.options}
-          onChange={newValue => (this.code = newValue)}
-        /> :
-          this.renderDiff()}
-
+        {!this.diff ? (
+          <MonacoEditor
+            class="full-editor"
+            value={this.code}
+            language={this.language}
+            theme={this.theme}
+            options={this.options}
+            onChange={newValue => (this.code = newValue)}
+          />
+        ) : (
+          this.renderDiff()
+        )}
       </div>
     )
   }
